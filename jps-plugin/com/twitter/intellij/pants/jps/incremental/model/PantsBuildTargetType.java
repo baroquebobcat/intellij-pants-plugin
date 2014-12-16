04b63b28e955d7fb381675be4a5b4e85e3c56c78
fkorotkov <fkorotkov@twitter.com>
2014-12-16 15:50:32 -0800

    Do not trigger pants goal compile if there is no dirty files.

--- jps-plugin/com/twitter/intellij/pants/jps/incremental/model/PantsBuildTargetType.java
+++ jps-plugin/com/twitter/intellij/pants/jps/incremental/model/PantsBuildTargetType.java
@@ -5,11 +5,13 @@ package com.twitter.intellij.pants.jps.incremental.model;
 
 import com.intellij.util.containers.ContainerUtil;
 import com.twitter.intellij.pants.jps.incremental.serialization.PantsJpsProjectExtensionSerializer;
+import com.twitter.intellij.pants.jps.util.PantsJpsUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jps.builders.BuildTargetLoader;
 import org.jetbrains.jps.builders.BuildTargetType;
 import org.jetbrains.jps.model.JpsModel;
+import org.jetbrains.jps.model.JpsProject;
 
 import java.util.List;
 
@@ -27,8 +29,9 @@ public class PantsBuildTargetType extends BuildTargetType<PantsBuildTarget> {
   }
 
   public PantsBuildTarget getTarget(JpsModel model) {
-    final JpsPantsProjectExtension projectExtension = PantsJpsProjectExtensionSerializer.findPantsProjectExtension(model.getProject());
-    return projectExtension != null ?
+    final JpsProject jpsProject = model.getProject();
+    final JpsPantsProjectExtension projectExtension = PantsJpsProjectExtensionSerializer.findPantsProjectExtension(jpsProject);
+    return projectExtension != null && PantsJpsUtil.containsPantsModules(jpsProject.getModules()) ?
            new PantsBuildTarget(projectExtension.getTargetPath(), projectExtension.getTargetNames()) : null;
   }
 
